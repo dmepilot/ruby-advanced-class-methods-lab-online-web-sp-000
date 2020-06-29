1a7a6ecfaafa560a8a7a34608c5d7af53178d055
Dean Erickson <dmepilot@yahoo.com>
2020-06-29 12:20:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,42 +15,42 @@ class Song
   def self.create
     song = self.new
     song.name = name
-    @@all << song
+    self.all << song
     song
   end
   
   def self.new_by_name(song_name)
     song = self.new
-    name = song_name
-    song.name = name
+    song.name = song_name
     song
   end
   
   def self.create_by_name(song_name)
     song = self.new
-    name = song_name
-    song.name = name
-    @@all << song
+    song.name = song_name
+    self.all << song
     song
   end
   
   def self.find_by_name(song_name)
-    new_array = []
-    @@all.each do |y|
-      new_array << y.name
-    end
-    if new_array.find {|x| x == song_name}
-    index = new_array.find_index {|x| x == song_name}
-    return @@all[index]
-    end
+  #  new_array = []
+   # @@all.each do |y|
+    #  new_array << y.name
+    #end
+    #if new_array.find {|x| x == song_name}
+    #index = new_array.find_index {|x| x == song_name}
+    #return @@all[index]
+    #end
+    self.all.find{|x| x.name == song_name}
   end
   
   def self.find_or_create_by_name(song_name)
-   if self.find_by_name(song_name) == nil
-     self.create_by_name(song_name)
-    else
-      self.find_by_name(song_name)
-   end
+  # if self.find_by_name(song_name) == nil
+   #  self.create_by_name(song_name)
+    #else
+     # self.find_by_name(song_name)
+   #end
+   self.find_by_name(song_name) || self.create_by_name(song_name)
   end
   
   def self.alphabetical
@@ -58,27 +58,21 @@ class Song
   end
   
   def self.new_from_filename(filename)
-    new_array = []
     new_array=filename.split(" - ")
     song_name=new_array[1].chop.chop.chop.chop
     artist=new_array[0]
     song = self.new
-    name = song_name
     song.name = song_name
-    artist_name = artist
     song.artist_name= artist
     song
   end
   
   def self.create_from_filename(filename)
-    new_array = []
     new_array=filename.split(" - ")
     song_name=new_array[1].chop.chop.chop.chop
     artist=new_array[0]
     song = self.new
-    name = song_name
     song.name = song_name
-    artist_name = artist
     song.artist_name= artist
     @@all << song
   end
